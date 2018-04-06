d05f639677e021f2942cac6df7e336358cdfd676
Anna <abuchman14@gmail.com>
2018-04-05 18:03:39 -0600

    Fixed stones fuck up

--- utils/setup_utils.py
+++ utils/setup_utils.py
@@ -10,7 +10,7 @@ def load_config(app_path_root):
     # Figure out what environment your on to see where to append the config path at.
     platform = sys.platform
     if platform in ['linux', 'darwin']:
-        app_path_root += '/configs/config.sample.yml'
+        config_path = app_path_root + '/configs/config.sample.yml'
     else:
         app_path_root += '\\configs\\config.sample.yml'
         config_path = app_path_root.lstrip('C:')
