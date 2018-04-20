33dbd1fe0b298d2eecd029148198d5f258f7bab1
Anna <abuchman14@gmail.com>
2018-04-20 02:03:35 -0600

    /analytics/con_id = pie chart

--- routes/analytics.py
+++ routes/analytics.py
@@ -2,7 +2,7 @@ from flask import Blueprint, g, render_template, request
 from utils.doc_utils import BlueprintDocumentation
 from utils.verify_utils import verify_issuer_jwt
 from models import requires_db
-
+from utils.utils import error_response
 
 analytics_bp = Blueprint('analytics', __name__)
 analytics_docs = BlueprintDocumentation(analytics_bp, 'Analytics')
@@ -26,19 +26,24 @@ def summary():
     return render_template('bar_chart.html', title='Tokens Claimed', max=100, labels=bar_labels, values=bar_values)
 
 
-@analytics_bp.route(url_prefix + "/pie", methods=['GET'])
+@analytics_bp.route(url_prefix + '/<con_id>', methods=['GET'])
 @verify_issuer_jwt
 @requires_db
-def pie():
-    data = g.sesh.execute("""
-    select contracts.*, (select count(*) from tokens where tokens.con_id = contracts.con_id and  status = 'S') as 
-    num_claimed from contracts where contracts.i_id = :desired_id;""", {'desired_id': g.issuer_info['i_id']}).fetchall()
+def percent_claimed(con_id):
 
-    labels = []
-    r = data[0]
-    claimed = ["Claimed", r['num_claimed']]
-    labels.append(claimed)
-    unclaimed = ["Unclaimed", r['num_created'] - r['num_claimed']]
-    labels.append(unclaimed)
-    return render_template('pie_chart.html', labels=labels)
+    data = g.sesh.execute("""select * from contracts where contracts.con_id = :contract_id;""",
+                           {"contract_id": con_id}).fetchall()
+    if len(data) == 0:
+        return error_response("Con id doesn't exist")
 
+    d_num_claimed = g.sesh.execute("""select count(*) as count1 from tokens where tokens.con_id=:contract_id 
+    and status='S';""", {'contract_id': con_id}).fetchall()
+
+    num_claimed = d_num_claimed[0]['count1']
+
+    d_num_created = g.sesh.execute("""select contracts.num_created as count2 from contracts where 
+    contracts.con_id = :contract_id;""", {"contract_id": con_id}).fetchall()
+    num_created = d_num_created[0]['count2']
+
+    values = [num_claimed, num_created - num_claimed]  # = num claimed, num unclaimed
+    return render_template('pie_chart.html', values=values)
