6e332a3df2ac3cf263bd9d2ce556671b05ec57a2
Stone <ssmele1995@gmail.com>
2018-10-16 00:14:06 -0600

    Endpoints added:     * Issuing trade request     * Delete Trade Request     * Accept trade request     * Decline trade request     * get all trade requests assocaited with collector.
    I havn't really tested these methods but a good majority of the code is
    in place and ready to be tested.
    
    Logic for the ethereum stuff needs to be placed in the /trade [PUT] method.

--- routes/trade.py
+++ routes/trade.py
@@ -2,11 +2,14 @@ from flask import Blueprint, g
 from flask_restful import Resource, Api
 
 from utils.doc_utils import BlueprintDocumentation
-from utils.utils import success_response, error_response
+from utils.utils import success_response, error_response, log_kv, LOG_INFO, LOG_ERROR
 from utils.db_utils import requires_db
 from utils.verify_utils import verify_collector_jwt
+from ether.geth_keeper import GethException
 from routes import load_with_schema
-from models.trade import TradeRequest, get_trades, create_trade_request
+from models.trade import TradeRequest, DeleteTradeRequest, TradeResponseRequest, GetTradeByTRID, UpdateTradeStatus, \
+    TradeStatus, GetTradeItems, InvalidateTradeRequests, GetActiveTradeRequests, \
+    create_trade_request, check_trade_item_ownership, check_active_trade_item, is_valid_trade_items
 
 trade_bp = Blueprint('trade', __name__)
 trade_docs = BlueprintDocumentation(trade_bp, 'Trade')
@@ -18,13 +21,38 @@ class Trade(Resource):
     @requires_db
     @verify_collector_jwt
     @load_with_schema(TradeRequest)
+    @trade_docs.document(url_prefix + ' ', 'POST', 'Method to issue trade request.', input_schema=TradeRequest,
+                         req_c_jwt=True)
     def post(self, data):
         try:
-            # TODO: Verify they own all tokens, Same with tradee.
             # Ensure the trader is the one making the request.
             if data['trader']['c_id'] != g.collector_info['c_id']:
-               return error_response('Not allowed to issue this trade request.')
+                return error_response('Not allowed to issue this trade request.')
 
+            # Ensure trader owns all tokens put up by trader.
+            for t_i in data['trader']['offers']:
+                if not check_trade_item_ownership(data['trader']['c_id'], t_i['con_id'], t_i['t_id']):
+                    log_kv(LOG_INFO, {'info': 'collector made trade request containing token they did not own.',
+                                      'trader_c_id': data['trader']['c_id'], 'con_id': t_i['con_id'],
+                                      't_id': t_i['t_id']})
+                    return error_response("Collector making request doesn't have ownership of tokens within trade")
+
+            # Ensure trader doesn't have any active trades containing put up tokens.
+            for t_i in data['trader']['offers']:
+                if check_active_trade_item(data['trader']['c_id'], t_i['con_id'], t_i['t_id']):
+                    log_kv(LOG_INFO, {'info': 'collector attempting to make trade on active token.',
+                                      'trader_c_id': data['trader']['c_id'], 'con_id': t_i['con_id'],
+                                      't_id': t_i['t_id']})
+
+            # Ensure tradee owns all tokens request
+            for t_i in data['tradee']['offers']:
+                if not check_trade_item_ownership(data['tradee']['c_id'], t_i['con_id'], t_i['t_id']):
+                    log_kv(LOG_INFO, {'info': 'collector made trade request containing token tradee did not own.',
+                                      'tradee_c_id': data['tradee']['c_id'], 'con_id': t_i['con_id'],
+                                      't_id': t_i['t_id']})
+                    return error_response("Collector making request for token the tradee doesn't have ownership of.")
+
+            # If we are dealing with a valid trade request persist it within the database.
             create_trade_request(data)
             g.sesh.commit()
             return success_response(resp_data={'trade_info': data},
@@ -36,8 +64,138 @@ class Trade(Resource):
 
     @requires_db
     @verify_collector_jwt
+    @load_with_schema(DeleteTradeRequest)
+    @trade_docs.document(url_prefix + '  ', 'DELETE', 'Method to cancel trade request.',
+                         input_schema=DeleteTradeRequest, req_c_jwt=True)
+    def delete(self, data):
+
+        # Get trade specified by requester.
+        trade = GetTradeByTRID().execute_n_fetchone(data)
+
+        # Make sure got a trade.
+        if trade is None:
+            return error_response('No trade request specified for that tr_id.')
+
+        # Ensure requester has access to cancel this trade.
+        if trade['trader_c_id'] != g.collector_info['c_id']:
+            log_kv(LOG_INFO, {'info': "Attempted deletion of trade request not owned by authorized collector",
+                              'c_id': g.gollector_info['c_id']})
+            return error_response('Authorized collector does not have ownership over this trade.')
+
+        # If identity has be verified update the status.
+        data.update({'new_status': TradeStatus.CANCELED.value})
+        if UpdateTradeStatus().execute(data):
+            g.sesh.commit()
+            return success_response(status='Trade request canceled.')
+        else:
+            g.sesh.rollback()
+            return error_response(status='Unable to cancel trade request.')
+
+    @requires_db
+    @verify_collector_jwt
+    @load_with_schema(TradeResponseRequest)
+    @trade_docs.document(url_prefix + '    ', 'PUT', 'Method to respond to trade request.',
+                         input_schema=TradeResponseRequest, req_c_jwt=True)
+    def put(self, data):
+        # Get trade specified by requester.
+        trade = GetTradeByTRID().execute_n_fetchone(data)
+
+        # Make sure got a trade.
+        if trade is None:
+            return error_response('No trade request specified for given tr_id.')
+
+        # Ensure requester has access to reject this trade.
+        if trade['tradee_c_id'] != g.collector_info['c_id']:
+            log_kv(LOG_INFO, {'info': "Attempted manipulation of trade request not directed to authorized collector.",
+                              'c_id': g.gollector_info['c_id']})
+            return error_response('Authorized collector is not the collector trade was intended for.')
+
+        # logic for accepting the request
+        if data['accept']:
+            # Get trade items specified by requester.
+            trade_items = GetTradeItems().execute_n_fetchall({'tr_id': trade['tr_id']})
+
+            # Check to make sure the trade_items are still valid.
+            if is_valid_trade_items(trade_items):
+
+                # If trade items are valid we can go through with the trade. Start by invalidating all trades
+                # containing items used within this trade.
+                for trade_item in trade_items:
+                    try:
+                        InvalidateTradeRequests().execute({'con_id': trade_item['con_id'], 't_id': trade_item['t_id']})
+                    except Exception as e:
+                        log_kv(LOG_ERROR, {'error': 'error invalidating trades containing trade items',
+                                           'con_id': trade_item['con_id'], 't_id': trade_item['t_id'],
+                                           'exception': str(e)}, exception=True)
+                        g.sesh.rollback()
+                        return error_response('Error accepting request.', http_code=400)
+
+                # Update trade request logic.
+                data.update({'new_status': TradeStatus.ACCEPTED.value})
+                if not UpdateTradeStatus().execute(data):
+                    g.sesh.rollback()
+                    return error_response(status='Error accepting request.')
+
+                # TODO: PLACE ETHEREUM LOGIC HERE. Most likely will work with trade_items object.
+                # Perform the logic on the block chain now.
+                try:
+                    pass
+                except GethException as e:
+                    g.sesh.rollback()
+                    return error_response('Error accepting request. [G]')
+                except Exception as e:
+                    g.sesh.rollback()
+                    return error_response('Error accepting request. [G]')
+
+                # If all the ethereum logic went through then commit database changes.
+                g.sesh.commit()
+                return success_response(status='Trade request accepted.')
+
+            else:
+                # Perform logic to decline the trade.
+                data.update({'new_status': TradeStatus.INVALID.value})
+                if UpdateTradeStatus().execute(data):
+                    g.sesh.commit()
+                    return success_response(status='Trade request declined.')
+                else:
+                    g.sesh.rollback()
+                    return error_response(status='Unable to decline trade request.')
+        # Logic for declining the request.
+        else:
+            # Perform logic to decline the trade.
+            data.update({'new_status': TradeStatus.DECLINED.value})
+            if UpdateTradeStatus().execute(data):
+                g.sesh.commit()
+                return success_response(status='Trade request declined.')
+            else:
+                g.sesh.rollback()
+                return error_response(status='Unable to decline trade request.')
+
+    @requires_db
+    @verify_collector_jwt
+    @trade_docs.document(url_prefix + '     ', 'GET', 'Method to get all active trade requests.', req_c_jwt=True)
     def get(self):
-        return success_response(resp_data={'trades': get_trades(g.collector_info['c_id'])})
+        # Get all tr_ids of active trade_requests containing the authorized collector..
+        tr_ids = GetActiveTradeRequests().execute_n_fetchall({'c_id': g.collector_info['c_id']})
+
+        trades = []
+        for tr_id in tr_ids:
+            # Get base trade info and all trade items associated.
+            trade = GetTradeByTRID().execute_n_fetchone({'tr_id': tr_id})
+            trade_items = GetTradeItems().execute_n_fetchall({'tr_id': tr_id})
+
+            # Go through and load object into desired format.
+            trades.append(TradeRequest().load({
+                'trader': {'c_id': trade['trader_c_id'],
+                           'offers': [{'con_id': t_i['con_id'], 't_id': t_i['t_id']} for t_i in trade_items
+                                      if t_i['owner'] == trade['trader_c_id']]},
+                'tradee': {'c_id': trade['tradee_c_id'],
+                           'offers': [{'con_id': t_i['con_id'], 't_id': t_i['t_id']} for t_i in trade_items
+                                      if t_i['owner'] == trade['tradee_c_id']]},
+                'tr_id': tr_id
+            }))
+
+        return success_response({'trades': trades})
 
 
 trade_api = Api(trade_bp)
